3058427ffe9ed24463395182fdc094350a95693a
dnt116302 <daotungalm@gmail.com>
2023-04-26 02:25:54 +0700

    Update 2023_04_25_180504_create_products_table.php

--- database/migrations/2023_04_25_180504_create_products_table.php
+++ database/migrations/2023_04_25_180504_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('price');
+            $table->string('feature_image_path')->nullable();
+            $table->text('content');
+            $table->integer('user_id');
+            $table->integer('category_id');
             $table->timestamps();
         });
     }
