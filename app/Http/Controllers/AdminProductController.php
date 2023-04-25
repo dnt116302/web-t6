605e4febf5d660f00002361c495357db3cff5d3b
dnt116302 <daotungalm@gmail.com>
2023-04-26 03:37:01 +0700

    Update AdminProductController.php

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Components\Recusive;
 
 class AdminProductController extends Controller
 {
+    private $category;
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+
     public function index(){
         return view('admin.product.index');
     }
 
     public function create(){
-        return view('admin.product.add');
+        $htmlOption = $this->getCategory($parentId ='');
+        return view('admin.product.add', compact('htmlOption'));
+    }
+
+    public function getCategory($parentId){
+        $data = $this->category->all();
+        $recusive = new Recusive($data);
+        $htmlOption = $recusive->categoryRecusive($parentId);
+        return $htmlOption;
     }
 }
