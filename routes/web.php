0c4966ca99e9dc5278d495a470027dca46cd67ba
dnt116302 <daotungalm@gmail.com>
2023-04-25 22:02:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'AdminController@loginAdmin');
+Route::get('/', 'App\Http\Controllers\AdminController@loginAdmin');
 
 Route::get('/home', function () {
     return view('Home');
