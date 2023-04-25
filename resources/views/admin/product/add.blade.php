644b3b2f3ebc58125695b7d5ee9ab58cf48c842f
dnt116302 <daotungalm@gmail.com>
2023-04-26 03:37:03 +0700

    Update add.blade.php

---
+++ resources/views/admin/product/add.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.admin')
+
+@section('title')
+    <title>Trang chủ</title>
+@endsection
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    @include('partial.content-header', ['name' => 'Product ', 'key' => 'Add'])
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <form action="" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label>Tên sản phẩm</label>
+                        <input type="text" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nhập tên sản phẩm">
+                      </div>
+                      <div class="form-group">
+                        <label>Chọn danh mục</label>
+                        <select class="form-control" name="parent_id" id="exampleFormControlSelect1">
+                          <option value="0">None</option>
+                            {{-- {!! $htmlOption !!} --}}
+                        </select>
+                      </div>
+
+                      <button type="Submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
+
