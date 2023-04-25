f8faf0b03863ac254903a4873263a3edf8e476d7
dnt116302 <daotungalm@gmail.com>
2023-04-26 01:06:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,66 +20,70 @@ Route::get('/home', function () {
     return view('home');
 });
 
-Route::prefix('categories')->group(function () {
-    Route::get('/', [
-        'as' => 'categories.index',
-        'uses' => 'App\Http\Controllers\CategotyController@index'
-    ]);
-
-    Route::get('/create', [
-        'as' => 'categories.create',
-        'uses' => 'App\Http\Controllers\CategotyController@create'
-    ]);
-
-    Route::post('/store', [
-        'as' => 'categories.store',
-        'uses' => 'App\Http\Controllers\CategotyController@store'
-    ]);
-
-    Route::get('/edit/{id}', [
-        'as' => 'categories.edit',
-        'uses' => 'App\Http\Controllers\CategotyController@edit'
-    ]);
-
-    Route::get('/delete{id}', [
-        'as' => 'categories.delete',
-        'uses' => 'App\Http\Controllers\CategotyController@delete'
-    ]);
-
-    Route::post('/update/{id}', [
-        'as' => 'categories.update',
-        'uses' => 'App\Http\Controllers\CategotyController@update'
-    ]);
+Route::prefix('admin')->group(function(){
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [
+            'as' => 'categories.index',
+            'uses' => 'App\Http\Controllers\CategotyController@index'
+        ]);
+    
+        Route::get('/create', [
+            'as' => 'categories.create',
+            'uses' => 'App\Http\Controllers\CategotyController@create'
+        ]);
+    
+        Route::post('/store', [
+            'as' => 'categories.store',
+            'uses' => 'App\Http\Controllers\CategotyController@store'
+        ]);
+    
+        Route::get('/edit/{id}', [
+            'as' => 'categories.edit',
+            'uses' => 'App\Http\Controllers\CategotyController@edit'
+        ]);
+    
+        Route::get('/delete{id}', [
+            'as' => 'categories.delete',
+            'uses' => 'App\Http\Controllers\CategotyController@delete'
+        ]);
+    
+        Route::post('/update/{id}', [
+            'as' => 'categories.update',
+            'uses' => 'App\Http\Controllers\CategotyController@update'
+        ]);
+    });
+    
+    Route::prefix('menus')->group(function () {
+        Route::get('/', [
+            'as' => 'menus.index',
+            'uses' => 'App\Http\Controllers\MenuController@index'
+        ]);
+    
+        Route::get('/create', [
+            'as' => 'menus.create',
+            'uses' => 'App\Http\Controllers\MenuController@create'
+        ]);
+    
+        Route::post('/store', [
+            'as' => 'menus.store',
+            'uses' => 'App\Http\Controllers\MenuController@store'
+        ]);
+    
+        Route::get('/edit/{id}', [
+            'as' => 'menus.edit',
+            'uses' => 'App\Http\Controllers\MenuController@edit'
+        ]);
+    
+        Route::post('/update/{id}', [
+            'as' => 'menus.update',
+            'uses' => 'App\Http\Controllers\MenuController@update'
+        ]);
+    
+        Route::get('/delete{id}', [
+            'as' => 'menus.delete',
+            'uses' => 'App\Http\Controllers\MenuController@delete'
+        ]);
+    });
 });
 
-Route::prefix('menus')->group(function () {
-    Route::get('/', [
-        'as' => 'menus.index',
-        'uses' => 'App\Http\Controllers\MenuController@index'
-    ]);
 
-    Route::get('/create', [
-        'as' => 'menus.create',
-        'uses' => 'App\Http\Controllers\MenuController@create'
-    ]);
-
-    Route::post('/store', [
-        'as' => 'menus.store',
-        'uses' => 'App\Http\Controllers\MenuController@store'
-    ]);
-
-    Route::get('/edit/{id}', [
-        'as' => 'menus.edit',
-        'uses' => 'App\Http\Controllers\MenuController@edit'
-    ]);
-
-    Route::post('/update/{id}', [
-        'as' => 'menus.update',
-        'uses' => 'App\Http\Controllers\MenuController@update'
-    ]);
-
-    Route::get('/delete{id}', [
-        'as' => 'menus.delete',
-        'uses' => 'App\Http\Controllers\MenuController@delete'
-    ]);
-});
