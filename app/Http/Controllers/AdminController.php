3fab5d823af6e13b9da7fd5c0c5f9e886d32149d
dnt116302 <daotungalm@gmail.com>
2023-04-26 00:51:42 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     //
     public function loginAdmin(){
         return view('login');
+    }
+
+    public function postloginAdmin(Request $request){
+        $remember = $request->has('remember-me') ? true : false;
+        if(auth()->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ], $remember)){
+            return view('home');
+        }
     }
 }
