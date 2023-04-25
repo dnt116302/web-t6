f5ceb548c8d70cd09eb1b3559c763d92dc4ab17a
dnt116302 <daotungalm@gmail.com>
2023-04-26 01:06:37 +0700

    Update MenuController.php

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -19,12 +19,12 @@ class MenuController extends Controller
 
     public function index(){
         $menus = $this->menu->paginate(10);
-        return view('menus.index', compact('menus'));
+        return view('admin.menus.index', compact('menus'));
     }
 
     public function create(){
         $optionSelect = $this->menuRecusive->menuRecusiveAdd();
-        return view('menus.add', compact('optionSelect'));
+        return view('admin.menus.add', compact('optionSelect'));
     }
 
     public function store(Request $request){
@@ -39,7 +39,7 @@ class MenuController extends Controller
     public function edit($id, Request $request){
         $menuFollowIdEdit = $this->menu->find($id);
         $optionSelect = $this->menuRecusive->menuRecusiveAdd();
-        return view('menus.edit', compact('menuFollowIdEdit', 'optionSelect'));
+        return view('admin.menus.edit', compact('menuFollowIdEdit', 'optionSelect'));
     }
 
     public function update($id, Request $request){
